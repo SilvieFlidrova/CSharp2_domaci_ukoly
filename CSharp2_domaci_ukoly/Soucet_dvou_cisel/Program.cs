5b1d74f0f4e58d0232fa852b9b2e8246340c9e92
SilvieFlidrova <flidrova@spsostrov.cz>
2024-04-25 17:43:03 +0200

    soucet dvou cisel

---
+++ CSharp2_domaci_ukoly/Soucet_dvou_cisel/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Soucet_dvou_cisel
+{
+//    Napište program, který se zeptá na dvě čísla a zobrazí jejich součet.
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ahoj, sečtu ti dvě čísla");
+
+            int prvniCislo = GetInputFromUser("Zadej první číslo");
+            int druheCislo = GetInputFromUser("Zadej druhé číslo");
+
+
+            Console.WriteLine($"Součet zadaných čísel {prvniCislo} a {druheCislo}je: {prvniCislo + druheCislo}");
+            int GetInputFromUser(string message)
+            {
+                int value;
+                bool isNumber;
+                do
+                {
+                    Console.WriteLine(message);
+                    string? input = Console.ReadLine();
+                    isNumber = int.TryParse(input, out value);
+                } while (isNumber is false);
+
+                return value;
+            }
+            Console.WriteLine();
+            return;
+            
+        }
+    }
+}
