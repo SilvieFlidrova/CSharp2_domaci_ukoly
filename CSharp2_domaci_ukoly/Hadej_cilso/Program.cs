c8c8295da7248af9de2769d6cfffedec16bf30c5
SilvieFlidrova <flidrova@spsostrov.cz>
2024-04-25 18:39:01 +0200

    hvezdicky

---
+++ CSharp2_domaci_ukoly/Hadej_cilso/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Hadej_cilso
+{
+    internal class Program
+    {
+       // Napište program, který umožní hádat číslo.Zeptá se, jaké číslo si myslím.Podle toho, jaké číslo se zadá, napíše, jestli je číslo větší nebo menší a umožní hádat tak dlouho, dokud se uživatel netrefí:
+       //Např.
+       //Hádej číslo: 10
+       //Číslo je menší, hádej znovu: 5
+       //Číslo je větší, hádej znovu: 7
+       //To je správně!
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ahoj, pojď hádat čísla!");
+
+            int hadaneCislo = 
+        }
+    }
+}
---
+++ CSharp2_domaci_ukoly/Hvezdicky/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Hvezdicky
+{
+    internal class Program
+    {
+        //Napište program, který se zeptá na počet hvězdiček a potom je v cyklu zobrazí na konzoli.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ahoj! Když mi zadáš počet hvězdiček, vykreslím je");
+
+            Console.WriteLine("zadej počet hvězdiček:");
+            string zadanyPocetHvezdicek = Console.ReadLine();
+            int pocetHvezdicek;
+            bool jeCislo = int.TryParse(zadanyPocetHvezdicek, out pocetHvezdicek);
+            if (!jeCislo)
+            {
+                Console.WriteLine("Nezadal jsi platné číslo");
+            };
+
+            for (int i = 1; i <= pocetHvezdicek; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
