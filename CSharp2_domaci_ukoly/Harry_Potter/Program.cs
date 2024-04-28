e744e42fb9f585a8993507fff43e8a4d7067cfd4
SilvieFlidrova <flidrova@spsostrov.cz>
2024-04-28 20:43:22 +0200

    spravna interpunkce na konci textu

--- CSharp2_domaci_ukoly/Harry_Potter/Program.cs
+++ CSharp2_domaci_ukoly/Harry_Potter/Program.cs
@@ -2,14 +2,14 @@
 {
     internal class Program
     {
-        
-    public static void Main()
-    {
-        // Tento ukol se opravuje sam. Kazdy priklad obsahuje kontrolni vypisy, ktere overi, ze jsi dosla k spravnemu vysledku.
-        // Vsechny priklady take obsahuji nejakou predpripravenou promennou, kam ulozis vysledek svoji prace.
 
-        int padding = 45;
-        string text = @"""Hurry up, boy!"" shouted Uncle Vernon from the kitchen. ""What are you
+        public static void Main()
+        {
+            // Tento ukol se opravuje sam. Kazdy priklad obsahuje kontrolni vypisy, ktere overi, ze jsi dosla k spravnemu vysledku.
+            // Vsechny priklady take obsahuji nejakou predpripravenou promennou, kam ulozis vysledek svoji prace.
+
+            int padding = 45;
+            string text = @"""Hurry up, boy!"" shouted Uncle Vernon from the kitchen. ""What are you
 doing, checking for letter bombs?"" He chuckled at his own joke.
 
 Harry went back to the kitchen, still staring at his letter. He handed
@@ -34,38 +34,38 @@ with one hand and glancing at it. His face went from red to green faster
 than a set of traffic lights. And it didn't stop there. Within seconds
 it was the grayish white of old porridge.";
 
-        // Nez zacnes volat nejake stringove funkce na nejake stringove promenne, nezapomen overit, ze obsahuje smysluplnou hodnotu. Vysledek uloz do promenne 'textMaSmysl'.
-        bool textMaSmysl = false;
-        Console.WriteLine("Text dava smysl - ".PadRight(padding) + (textMaSmysl == true));
+            // !Nez zacnes volat nejake stringove funkce na nejake stringove promenne, nezapomen overit, ze obsahuje smysluplnou hodnotu. Vysledek uloz do promenne 'textMaSmysl'.
+            bool textMaSmysl = false;
+            Console.WriteLine("Text dava smysl - ".PadRight(padding) + (textMaSmysl == true));
 
-            // Do promenne 'delkaTextu' uloz celkovou delku uryvku z knizky.
+            // !Do promenne 'delkaTextu' uloz celkovou delku uryvku z knizky.
 
             char[] pocetZnaku = text.ToCharArray();
-        int delkaTextu = pocetZnaku.Length;
+            int delkaTextu = pocetZnaku.Length;
 
-        Console.WriteLine(delkaTextu);
-        Console.WriteLine("Delka text je spravna - ".PadRight(padding) + (delkaTextu == 1001));
+            Console.WriteLine(delkaTextu);
+            Console.WriteLine("Delka text je spravna - ".PadRight(padding) + (delkaTextu == 1001));
 
-        // Do promenne 'oddelovac' vloz text, ktery se sklada pouze z pomlcek a jeho delka je presne 20. Pouzij k tomu konstruktor typu string.
+            // Do promenne 'oddelovac' vloz text, ktery se sklada pouze z pomlcek a jeho delka je presne 20. Pouzij k tomu konstruktor typu string.
 
-        string oddelovac = new string ('-',20);
-        Console.WriteLine("Oddelovac ma 20 pomlcek - ".PadRight(padding) + (oddelovac == "--------------------"));
+            string oddelovac = new string('-', 20);
+            Console.WriteLine("Oddelovac ma 20 pomlcek - ".PadRight(padding) + (oddelovac == "--------------------"));
 
-        // Pozmen nasledujici prvniPorovnani textu tak, aby se do konzole vypisovalo True, aniz bys menila hodnoty promennych
+            // Pozmen nasledujici prvniPorovnani textu tak, aby se do konzole vypisovalo True, aniz bys menila hodnoty promennych
 
-        string jmeno1 = "Katka";
-        string jmeno2 = "katka";
-        bool jeStejne = string.Equals(jmeno1, jmeno2, StringComparison.InvariantCultureIgnoreCase);
-        Console.WriteLine("Obe promenne obsahuji stejne jmeno - ".PadRight(padding) + jeStejne);
+            string jmeno1 = "Katka";
+            string jmeno2 = "katka";
+            bool jeStejne = string.Equals(jmeno1, jmeno2, StringComparison.InvariantCultureIgnoreCase);
+            Console.WriteLine("Obe promenne obsahuji stejne jmeno - ".PadRight(padding) + jeStejne);
 
-        // Zjisti, jestli je v textu zminka o obtloustle "tete" Harryho. Jmenuje se Marge. Vysledek uloz do promenne 'piseSeOMarge';
-        
-        bool piseSeOMarge = text.Contains("Marge");
-        Console.WriteLine("V textu se zminuje Harryho 'teticka' - ".PadRight(padding) + (piseSeOMarge == true));
+            // Zjisti, jestli je v textu zminka o obtloustle "tete" Harryho. Jmenuje se Marge. Vysledek uloz do promenne 'piseSeOMarge';
 
-        // Zjisti, jestli je text spravne ukonceny interpunkci. Vysledek uloz do promenne 'jeSpravneUkoncen'.
+            bool piseSeOMarge = text.Contains("Marge");
+            Console.WriteLine("V textu se zminuje Harryho 'teticka' - ".PadRight(padding) + (piseSeOMarge == true));
 
-        bool jeSpravneUkoncen = false;
+            // Zjisti, jestli je text spravne ukonceny interpunkci. Vysledek uloz do promenne 'jeSpravneUkoncen'.
+
+            bool jeSpravneUkoncen = text.EndsWith('.') || text.EndsWith('!') || text.EndsWith('?');
         Console.WriteLine("Text je spravne ukoncen interpunkci - ".PadRight(padding) + (jeSpravneUkoncen == true));
 
         // Pomoci abecedniho porovnavani zjisti, ktery z nasledujicich textu je podle abecedy prvni a jeho hodnotu prirad do promenne 'prvni'.
@@ -106,13 +106,9 @@ it was the grayish white of old porridge.";
                 
             }
 
-           
-
-
-
             Console.WriteLine("Prvni v abecede je blabol3 - ".PadRight(padding) + (prvni == blabol3));
 
-        // Najdi prvni rozkazovaci vetu v textu a uloz ji do promenne 'veta' bez vykricniku a uvozovek.
+        // !Najdi prvni rozkazovaci vetu v textu a uloz ji do promenne 'veta' bez vykricniku a uvozovek.
         
         int indexVykricnik = text.IndexOf("!");
             Console.WriteLine(indexVykricnik);
